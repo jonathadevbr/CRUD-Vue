0e472446007146be8b8bf5918bb5a5837be59b69
jonathadevbr <jhonatavianna191@gmail.com>
2026-02-04 12:06:22 -0300

    Subindo projeto

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// Importa o CSS do Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'
+// Importa o JS do Bootstrap (importante para modais e dropdowns)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+import './assets/main.css'
+
+import Home from './page/Home.vue';
+</script>
+
+<template>
+  <Home/>
+</template>
---
+++ src/page/Home.vue
@@ -0,0 +1,69 @@
+<script>
+
+
+</script>
+<template>
+  <form class="formularioCadastro">
+    <div class="mb-3">
+      <label class="form-label">Nome:</label>
+      <input type="text" class="form-control" id="inputNome">
+    </div>
+    <div class="mb-3">
+      <label class="form-label">Telefone</label>
+      <input type="number" class="form-control" id="inputTelefone">
+    </div>
+    <div class="mb-3 form-check"> 
+      <label class="form-check-label" >Se for maior de idade marque a caixa</label>
+      <input type="checkbox" class="form-check-input" id="inputMaiorIdade">
+    </div>
+    <button type="submit" class="btn btn-primary">Adicionar</button>
+  </form>
+  <nav class="navbar bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand">Pesquisar por</a>
+      <form class="d-flex" role="search">
+        <input v-model="nome" class="form-control me-2" type="search" placeholder="Nome" aria-label="Nome" />
+        <input v-model="cpf" class="form-control me-2" type="search" placeholder="CPF" aria-label="Cpf" />
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </nav>
+  <table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Nome</th>
+        <th scope="col">Telefone</th>
+        <th scope="col">Maior idade</th>
+        <th scope="col">Editar</th>
+        <th scope="col">Excluir</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+        <td><button>Editar</button></td>
+        <td><button>Excluir</button></td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+        <td><button>Editar</button></td>
+        <td><button>Excluir</button></td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td>John</td>
+        <td>Doe</td>
+        <td>@social</td>
+        <td><button>Editar</button></td>
+        <td><button>Excluir</button></td>
+      </tr>
+    </tbody>
+  </table>
+</template>
